ef545507feb218a280561de5e85f48c92317f385
Ashish <ashish9342@gmail.com>
2024-05-25 02:46:08 +0530

    integrate services with backend

---
+++ client/src/components/Comment/List.jsx
@@ -0,0 +1,36 @@
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default ({ postId }) => {
+  const [comments, setComments] = useState([]);
+
+  const fetchComments = async () => {
+    const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
+    setComments(res.data);
+  };
+
+  useEffect(() => {
+    fetchComments();
+  }, []);
+
+  const renderedComments = comments.map((comment) => {
+    // let content;
+
+    // if (comment.status === 'approved') {
+    //   content = comment.content;
+    // }
+
+    // if (comment.status === 'pending') {
+    //   content = 'This comment is awaiting moderation';
+    // }
+
+    // if (comment.status === 'rejected') {
+    //   content = 'This comment has been rejected';
+    // }
+
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+};
